fb45b49bbf16a4c76788cfe5febd69a662aea087
학영 <gkr097@naver.com>
2024-02-26 21:21:57 +0900

    Add get_Abs function

--- calculator.py
+++ calculator.py
@@ -11,11 +11,14 @@ def multiply(a, b):
 def divide_premium(a, b):
   return a / b
 
-
 def getMedian(a, b):
     return (a+b)/2
 
 def getRemainder(a, b):
   return a//b
 
-
+def get_Abs(num):
+  if num>=0:
+    return num
+  else:
+    return -num
