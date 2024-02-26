f906fc2f16d3212a2ab8772537f6dab7a7db437c
학영 <gkr097@naver.com>
2024-02-26 16:09:58 +0900

    Add get_Median function

--- calculator.py
+++ calculator.py
@@ -3,7 +3,6 @@
 def add(a, b):
   return a+b
 def subtract(a, b):
-<<<<<<< HEAD
   return a-b
 
 def multiply(a, b):
@@ -11,6 +10,7 @@ def multiply(a, b):
 
 def divide_premium(a, b):
   return a / b
-=======
-  return a-b
->>>>>>> parent of bf525e6 (add the info of calculator.py in README.md)
+
+def getMedian(a, b):
+    return (a+b)/2
+
